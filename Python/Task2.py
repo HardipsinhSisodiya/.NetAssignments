1790e5674caf94c91b634baf97d072d5104de7a5
HardipsinhSisodiya <78913067+HardipsinhSisodiya@users.noreply.github.com>
2021-10-20 15:51:24 +0530

    Add files via upload

---
+++ Python/Task2.py
@@ -0,0 +1,9 @@
+
+a=[]
+for Item in range(5):
+    print(Item ," Username:")
+    a.append(input())
+#for Item in range(a.count):
+print("Usernames: ",end=" ")
+for x in range(5):
+    print(a[x],end=" ")
---
+++ Python/Task3.py
@@ -0,0 +1,2 @@
+Num1,Num2=input("Enter two numbers\n").split()
+print(int(Num1)*int(Num2))
